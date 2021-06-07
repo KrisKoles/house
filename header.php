87ffccff410cde6cf46ade3c5a015fbd781b78b2
KrisKoles <62072797+KrisKoles@users.noreply.github.com>
2021-06-07 17:00:23 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php bloginfo( 'description' ); ?></title>  <!-- название сайта может менять заказчик в настройках вп -->
+    <!-- <link rel="preconnect" href="https://fonts.gstatic.com"> -->
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="css/style.css" /> -->
+    <?php wp_head(); ?>
+  </head>
+
+  <body>
+    <header class="header" style="background-image: url(<?php the_field('top__bg')?>);">
+      <div class="header__inner">
+        <img src="<?php bloginfo( 'template_url' ); ?>/assets/img/home.png" alt="" />
+          <div class="header__name">
+            <?php the_field('header__name')?> <!-- изменяемое название-->
+          </div>
+          <a class="phone" href="tel:+75852772217">
+            <?php the_field('phone')?>
+          </a>
+          <div class="header__title">
+            <?php the_field('header__title')?>
+          </div>
+        <div class="header__sale">
+          <img src="<?php the_field('header__sale')?>" alt="" />
+        </div>
+      </div>
+    </header>
